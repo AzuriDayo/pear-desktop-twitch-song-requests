888e3d985f408183484f98338c56567d099d8fe2
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2025-12-07 04:36:39 -0500

    feat: add release pipeline

--- build_dist.sh
+++ build_dist.sh
@@ -8,7 +8,7 @@ for os in "${osfname[@]}"; do
     for arch in "${archfname[@]}"; do
         execname=${appname}_${os}_${arch}
         if [ "$os" = 'windows' ]; then execname=$execname.exe ; fi
-        GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -trimpath -o "$execname" cmd/main/main.go &
+        GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -trimpath -o "$execname" cmd/main/*.go &
     done
 done
 
