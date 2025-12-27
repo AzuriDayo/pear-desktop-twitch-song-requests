637743a8b98b3d9c80aa051f9c3d96343e4f5c53
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2025-12-27 14:36:27 -0500

    feat: add nightly builds

--- build_dist.sh
+++ build_dist.sh
@@ -9,7 +9,15 @@ go mod download
 for os in "${osfname[@]}"; do
     for arch in "${archfname[@]}"; do
         execname=${appname}_${os}_${arch}
-        if [ "$os" = 'windows' ]; then execname=$execname.exe ; fi
+        if [ "$os" = 'windows' ] && [ "$arch" = 'arm64' ]; then
+            execname=${appname}_${os}_on_arm.exe
+        elif [ "$os" = 'windows' ] && [ "$arch" = 'amd64' ]; then
+            execname=${appname}_${os}_x64.exe
+        elif [ "$os" = 'darwin' ] && [ "$arch" = 'arm64' ]; then
+            execname=${appname}_macos_apple_silicon.exe
+        elif [ "$os" = 'darwin' ] && [ "$arch" = 'amd64' ]; then
+            execname=${appname}_macos_intel.exe
+        fi
         CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${GIT_SHORT_TAG}" -trimpath -o "$execname" ./cmd/main &
     done
 done
