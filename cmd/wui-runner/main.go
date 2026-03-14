16561500793c8329e01d01fa236972069f872e40
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2026-03-13 22:20:40 -0400

    chore: add small windows ui app

---
+++ cmd/wui-runner/main.go
@@ -0,0 +1,16 @@
+//go:build windows
+
+package main
+
+import (
+	"os/exec"
+
+	"github.com/ChromeTemp/Popup"
+)
+
+func main() {
+	b := Popup.LazyDialog("Open github", "Do you want to open github?")
+	if b {
+		exec.Command("cmd", "/c", "start", "https://github.com/AzuriDayo_/pear-desktop-twitch-song-requests").Start()
+	}
+}
