8d19111934e95222dbe3f33be12b33c7a09192eb
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2026-05-18 21:47:34 -0400

    chore: replace alias to corresponding command

--- build-frontend.ps1
+++ build-frontend.ps1
@@ -1,4 +1,4 @@
-cd "$(git rev-parse --show-toplevel)"
+Set-Location "$(git rev-parse --show-toplevel)"
 pnpm run -r build
 node -e "require('fs').rmSync('./cmd/main/build', { recursive: true, force: true, maxRetries: process.platform === 'win32' ? 10 : 0 })"
-cp -r control-panel/build cmd/main
+Set-Location -r control-panel/build cmd/main
